55c60de74d3a9f957be340ea76db19ef9e88b3e2
Matthew Klemmer <theklemmercode@outlook.com>
2023-06-29 22:06:59 -0400

    Ready for prod.

--- src/assets/components/Quiz.jsx
+++ src/assets/components/Quiz.jsx
@@ -1,32 +1,41 @@
 import { useEffect, useState } from 'react'
 import '../css/Quiz.css'
 
-function Quiz({ handleChoice, quiz }) {  
+function Quiz({ handleChoice, quiz, stage }) {
 
-    const questionElements = quiz?.map((question, index) => {
-        const answersElements = question.answers.map((answer, index) => {
-            return(
-                <button 
-                    className={`answer-button ${answer.selected ? 'answer-selected' : ''}`}
-                    onClick={() => handleChoice(answer.id)}
-                    key={answer.id}
-                >
-                    {answer.text}
-                </button>
-            )
-        })
-        return(
-            <div key={question.id} className='quiz-question'>
-                <p>{question.question}</p>
-                <div className='quiz-answers'>
-                    {answersElements}
-                </div>
-            </div>
-        )})
-    
+  const questionElements = quiz?.map((question, index) => {
+    const answersElements = question.answers.map((answer, index) => {
+      let addedClass = ""
+      if (stage === "final" && answer.selected && answer.correct) {
+        addedClass = "answer-correct"
+      } else if (stage === "final" && !answer.selected && answer.correct) {
+        addedClass = "actual-answer"
+      } else if (answer.selected) {
+        addedClass = "answer-selected"
+      }
+      return (
+        <button
+          className={`answer-button ${addedClass}`}
+          onClick={() => handleChoice(answer.id)}
+          key={answer.id}
+        >
+          {answer.text}
+        </button>
+      )
+    })
+    return (
+      <div key={question.id} className='quiz-question'>
+        <h3 className='question-text'>{question.question}</h3>
+        <div className='quiz-answers'>
+          {answersElements}
+        </div>
+      </div>
+    )
+  })
+
   return (
-    <section className='quizzical-quiz'>
-        {questionElements}
+    <section className='quizitive-quiz'>
+      {questionElements}
     </section>
   )
 }
---
+++ src/assets/components/QuizLoader.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+import '../css/QuizLoader.css'
+
+function QuizLoader() {
+  return (
+    <span class="loader"></span>
+  )
+}
+
+export default QuizLoader
